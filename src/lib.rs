f3afb8eedd48da6824416d17282f840d259ef015
Wyatt Mattas <wyatt.mattas@invista.com>
2024-06-21 16:10:48 -0500

    removed unnecessary imports

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,7 @@
 #![no_std]
 
-//mod bmi3_core;
-//pub mod bmi3_defs;
-//pub mod bmi3_macros;
-//pub mod bmi3_sens_axes;
-//mod bmi3_types;
-//pub mod bmi3dev;
-//pub mod bmi3mapint;
-//pub mod config;
 pub mod device;
-//pub mod enums;
-//mod get_sensor_config;
 mod interface;
-//mod set_accel_sensor_config;
-//mod set_gyro_sensor_config;
 mod registers;
 pub use registers::Register;
 mod types;
